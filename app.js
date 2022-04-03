78b02d3b8951b7daeddad8a4a1073bc62cd56bbb
D Meeks <Dj.meeks@yahoo.com>
2022-04-02 19:29:52 -0700

    final upload before review

--- app.js
+++ app.js
@@ -24,72 +24,75 @@
 */
 const navigation = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
-const link = document.querySelectorAll('.navbar_menu a');
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+const fragment = document.createDocumentFragment();
+// I was using this for the scrolling feature but did it with html css instead.
+// const link = document.querySelectorAll('.navbar_menu a');
 
 
+// build the nav out completely using the list  and menu link to append to navUI array.
+function navBuildBar() {
+    // storing the information for the sections
+    let navBar = '';
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-const navBuildBar = () => {
-
-    let navUI = '';
-  
     sections.forEach(section => {
+        //defining the variables to feed into the navUI list by pulling section ID and datasets
+        const secListId = section.id;
+        const navData = section.dataset.nav;
 
-        const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
-
-        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+        navBar += `<li><a class="menu__link" href="#${secListId}">${navData}</a></li>`;
 
     });
-  
-   navigation.innerHTML = navUI;
+    //   Appending this to the navBar
+     navigation.innerHTML = navBar;
+  fragment.append(navBar);
 
-
-
-};
-
+}
+// calling the navbar function before doing the next set of code.
 navBuildBar();
 
+// getting the information from the viewport to feed into the active and none active state
  const offset =  function isInViewport (section) {
     return Math.floor( section.getBoundingClientRect().top);
     
  };
-
+// removing the active status when it is no longer in the view port, defining the const
 const removeActive =(section) => {
+    // removing the section from the class list
     section.classList.remove("your-active-class");
+    //changing the color to black when not active in the viewport
     section.style.backgroundColor = "black";
 };
+//making the the section active when it shows up in the view port setting the const
 const addActive = (conditional, section) => {
+    //if statement for setting the class active and adding to the classlist
     if(conditional){ section.classList.add("your-active-class");
-    section.style.backgroundColor = "white";
+    //changing the color to indicate the section is currently active in the viewport
+    section.style.backgroundColor = "yellow";
 };
 }
-    
+ // this function is the loop for the sections activation   
 function sectionActivation() {
     sections.forEach(sections => {
+
+    //setting the constant for offset number for the add active 
         const elementOffset = offset(sections);
+        // this is setup to set the actual value to passed later into the addActive 
         isInViewport = () => elementOffset < 100 && elementOffset >= -100;
+        //this removes the section once it is outside of the above parameters
         removeActive(sections);
+        // this uses the previous defined parameters to add a section to the viewport
         addActive(isInViewport(), sections);
 
     });
 };
 
-
+//adding listeners for the scroll feature and the activation functions
 
 window.addEventListener('scroll', sectionActivation);
+
+                    //Below are attempts at adding the scroll via javascript//
+
+
 // In this one I am trying to target the id and pass it in to control the scrolling. 
 // const smoothScroll = document.getElementById('navbar_list').addEventListener('click', function (e) {
 //     e.preventDefault();
