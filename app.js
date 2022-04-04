ece9346bb1adf60aee86003c001376faf9ea346f
D Meeks <Dj.meeks@yahoo.com>
2022-04-03 17:43:17 -0700

    4th fix just need the scroll bar

--- app.js
+++ app.js
@@ -42,82 +42,94 @@ const navBarBuilder=() => {
 
     };
 };
-    // attempts to append various elements
-    // menuList.appendChild(navBar);
-    // document.appendChild(menuList);
-    // menuList.appendChild(fragment);
-
-    // navigationBar.innerHTML = navBar;
-    // element.appendChild(navBarList);
-    // element.appendChild(menuNav,menuA,menuList);
-
+  
 
 navBarBuilder();
 
-const offset =  function isInViewport (section) {
-return Math.floor( section.getBoundingClientRect().top);
-};
+// function removeActive(section) {
+//     // removing the section from the class list
+//     section.classList.remove("your-active-class");
+//     //changing the color to black when not active in the viewport
+//     section.style.backgroundColor = "black";
+// }
+// //making the the section active when it shows up in the view port setting the const
+// const addActive = (conditional, section) => {
+//     //if statement for setting the class active and adding to the classlist
+//     if(conditional){ section.classList.add("your-active-class");
+//     //changing the color to indicate the section is currently active in the viewport
+//     section.style.backgroundColor = "yellow";
+// };
+// };
 
-                            //    Old Code building nav ////
-// // build the nav out completely using the list  and menu link to append to navUI array.
-// function navBuildBar() {
-//     // storing the information for the sections
-//     let navBar = '';
+function isInViewport(view) {
+    const rect = view.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+sectionActivation =()=> {
+    sections.forEach(section => {
+        let classList = section.classList;
+               if (isInViewport(section)){
+            section.classList.add("your-active-class");
+let activeView = document.getElementsByClassName(classList);
+         section.style.backgroundColor ="black";
 
+            }else{
+            section.classList.remove("your-active-class");
+            section.style.backgroundColor = "blue";
+        }
+     });
+            
+    };
+//removing the active status when it is no longer in the view port, defining the const
+// function removeActive(section) {
+//     // removing the section from the class list
+//     section.classList.remove("your-active-class");
+//     //changing the color to black when not active in the viewport
+//     section.style.backgroundColor = "black";
+// }
+// //making the the section active when it shows up in the view port setting the const
+// const addActive = (conditional, section) => {
+//     //if statement for setting the class active and adding to the classlist
+//     if(conditional){ section.classList.add("your-active-class");
+//     //changing the color to indicate the section is currently active in the viewport
+//     section.style.backgroundColor = "yellow";
+// };
+// };
+
+// sectionActivation =()=> {
 //     sections.forEach(section => {
-//         //defining the variables to feed into the navUI list by pulling section ID and datasets
-//         const secListId = section.id;
-//         const navData = section.dataset.nav;
+//                if (isInViewport(section)){
+//             section.classList.add("your-active-class");
 
-//         // navBar += `<li><a class="menu__link" href="#${secListId}">${navData}</a></li>`;
+//             }else{
+//             section.classList.remove("your-active-class");
+//         }
+//      });
+            
+//     };
+
+// addActive(isInViewport(), sections);
+
+ // this function is the loop for the sections activation   
+// function sectionActivation() {
+//     sections.forEach(sections => {
+
+//     //setting the constant for offset number for the add active 
+//         const elementOffset = offset(sections);
+//         // this is setup to set the actual value to passed later into the addActive 
+//         isInViewport = () => elementOffset < 100 && elementOffset >= -100;
+//         //this removes the section once it is outside of the above parameters
+//         removeActive(sections);
+//         // this uses the previous defined parameters to add a section to the viewport
+//         addActive(isInViewport(), sections);
 
 //     });
-//     //   Appending this to the navBar
-//     //  navigationBar.innerHTML = navBar;
-//   fragment.append(navBar);
-
-
-// calling the navbar function before doing the next set of code.
-// navBuildBar();
-
-
-
-
-//getting the information from the viewport to feed into the active and none active state
-//  const offset =  function isInViewport (section) {
-//     return Math.floor( section.getBoundingClientRect().top);
-    
-
-//removing the active status when it is no longer in the view port, defining the const
-function removeActive(section) {
-    // removing the section from the class list
-    section.classList.remove("your-active-class");
-    //changing the color to black when not active in the viewport
-    section.style.backgroundColor = "black";
-}
-//making the the section active when it shows up in the view port setting the const
-const addActive = (conditional, section) => {
-    //if statement for setting the class active and adding to the classlist
-    if(conditional){ section.classList.add("your-active-class");
-    //changing the color to indicate the section is currently active in the viewport
-    section.style.backgroundColor = "yellow";
-};
-}
- // this function is the loop for the sections activation   
-function sectionActivation() {
-    sections.forEach(sections => {
-
-    //setting the constant for offset number for the add active 
-        const elementOffset = offset(sections);
-        // this is setup to set the actual value to passed later into the addActive 
-        isInViewport = () => elementOffset < 100 && elementOffset >= -100;
-        //this removes the section once it is outside of the above parameters
-        removeActive(sections);
-        // this uses the previous defined parameters to add a section to the viewport
-        addActive(isInViewport(), sections);
-
-    });
-};
+// };
 
 //adding listeners for the scroll feature and the activation functions
 
