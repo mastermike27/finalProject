2498d95a80c749b9f68e15f1ba54667d69729865
D Meeks <Dj.meeks@yahoo.com>
2022-04-24 12:28:52 -0700

    upload for viewport rework

--- js/app.js
+++ js/app.js
@@ -51,15 +51,11 @@ navBarBuilder();
 
 // function to get the position in the view port
 // this code is pulled from open source not another students work //////////////////////////////////
-//referred from javascripttutorial.net https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+//referred from javascripttutorial.net && https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport 4/25/2022
 function isInViewport(view) {
     const rect = view.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+     return Math.abs(rect.top >= 0 && window.innerHeight) <= rect.bottom && window.innerWidth;
+    
 }
 //function to activate or apply or remove the active class depending on position
 sectionActivation =()=> {
