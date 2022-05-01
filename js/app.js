e684929e8fc68a2c57563b675469561b5c511b44
D Meeks <Dj.meeks@yahoo.com>
2022-04-30 21:21:12 -0700

    added loop for the sections

--- js/app.js
+++ js/app.js
@@ -19,14 +19,34 @@
 */
 
 /**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
  * Define Global Variables
  * 
 */
 //variables to get the information from the nav and sections 
 const navigationBar = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
-const btn = document.getElementById("menu_link");
 
+const navLi = document.querySelectorAll(".navbar__menu .menu__link");
 
 
 //building out the navigation functions
@@ -36,16 +56,37 @@ const navBarBuilder=() => {
   for(let i = 0; i < sections.length; i++) {
       //creating the li element to append the nav too
     const menuList = document.createElement('li');
+    
     //getting id and data nav information to pass into the menulist inner html
     const secListId = sections[i].getAttribute('id');
     const navData =sections[i].getAttribute('data-nav');
-    menuList.innerHTML= `<a class="menu__link" "active" href="#${secListId}">${navData}</a>`;
+    menuList.innerHTML= `<a class="menu__link"  href="#${secListId}">${navData}</a>`;
     //appending the li, menulist to the navigation bar
     navigationBar.appendChild(menuList);
+   
 
     };
+    
 };
 
+window.addEventListener("scroll", () => {
+  let current = "";
+  section.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+  console.log(current);
+});
 //build navigation bar
 navBarBuilder();
 
@@ -76,7 +117,7 @@ sectionActivation =()=> {
         console.log(isInViewport(section));
                if (isInViewport(section)){
             section.classList.add("your-active-class");
-            
+         
 //'changing color of the class when active in view port
         section.style.backgroundColor ="black";
        
@@ -86,7 +127,7 @@ sectionActivation =()=> {
             }else{
             section.classList.remove("your-active-class");
             //setting color for when not i the view port
-            // section.style.backgroundColor = "blue";
+            section.style.backgroundColor = "blue";
         }
      });
             
@@ -105,5 +146,3 @@ sectionActivation =()=> {
 window.addEventListener('scroll', sectionActivation);
 
     })
-
-   
